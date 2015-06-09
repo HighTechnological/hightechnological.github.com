35104210ca826c27a0dff53426e6a8f039e503b3
travis <travis@browser.html>
2015-06-09 16:51:32 +0600

    Новый лаунчер
    Citrus Watch OS 0.2.0
    Новый список приложений

--- AppList.js
+++ AppList.js
@@ -2,22 +2,31 @@ var appList = [{
 	"name":"watch",
 	"iconType":IconType.ANIMATE
 }, {
-	"name":"naver",
-	"iconType":IconType.IMAGE
-}, {
-	"name":"githubrepo",
-	"iconType":IconType.IMAGE
-}, {
 	"name":"calculator",
 	"iconType":IconType.IMAGE
 }, {
 	"name":"weather",
 	"iconType":IconType.IMAGE
-	}, {
-	"name":"clock",
+}, {
+	"name":"AppleWatch",
 	"iconType":IconType.IMAGE
-	}, {
+}, {
 	"name":"desktop",
 	"iconType":IconType.IMAGE
-	}, {
+}, {
+	"name":"bus",
+	"iconType":IconType.IMAGE
+}, {
+	"name":"clock",
+	"iconType":IconType.IMAGE
+}, {
+	"name":"settings",
+	"iconType":IconType.IMAGE
+}, {
+	"name":"music",
+	"iconType":IconType.IMAGE
+}, {
+	"name":"flatcalendar",
+	"iconType":IconType.IMAGE
+}, {
 }];
